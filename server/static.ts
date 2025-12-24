dd95b62cc57841b79c71d3090a5a8a9e19b11adc
addyj4922 <adityaj518@gmail.com>
2025-12-24 20:33:33 +0530

    NA

--- server/static.ts
+++ server/static.ts
@@ -17,6 +17,7 @@ export function serveStatic(app: Express) {
   if (fs.existsSync(assetsPath)) {
     app.use("/attached_assets", express.static(assetsPath));
   }
+
   
   // fall through to index.html if the file doesn't exist
   app.use("*", (_req, res) => {
